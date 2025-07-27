22ab323df31e010a7d204f5a3e23f765ca3d4edd
AdrianaRodriguez7 <137732661+AdrianaRodriguez7@users.noreply.github.com>
2025-07-26 23:09:44 -0400

    .

---
+++ ApiEjemplo/ApiEjemplo/Controllers/ProductosController.cs
@@ -0,0 +1,51 @@
+﻿using ApiEjemplo.Data;
+using Microsoft.AspNetCore.Mvc;
+using ApiEjemplo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEjemplo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductosController : ControllerBase
+
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get: api/productos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> Get()
+        
+            {
+                return await _context.Productos.ToListAsync();
+            }
+
+        
+
+        //POST: api/productos
+        [HttpPost]
+        public async Task<ActionResult<Producto>> Post(Producto producto)
+        {
+            _context.Productos.Add(producto);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = producto.Id }, producto);
+        }
+
+        //PUT: api/productos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Producto producto)
+        {
+            if (id != producto.Id)
+                return BadRequest();
+
+            _context.Entry(producto).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
