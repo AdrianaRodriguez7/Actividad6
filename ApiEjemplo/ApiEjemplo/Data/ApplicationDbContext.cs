f1ae97333e47d927d561fdbececefdb3c4e415af
AdrianaRodriguez7 <137732661+AdrianaRodriguez7@users.noreply.github.com>
2025-07-26 21:13:01 -0400

    Agregando modelos y datos, excluyendo archivos temporales

---
+++ ApiEjemplo/ApiEjemplo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+//Creo una clase que representa la conexión con la base de datos y
+//define las tablas mediante DbSet.
+using ApiEjemplo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEjemplo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) 
+            : base(options) { }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ ApiEjemplo/ApiEjemplo/Models/Producto.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ApiEjemplo.Models
+{
+    public class Producto
+    {
+        //definiendo la clase que representa los datos que se guardarán en la base de datos
+        public int Id {  get; set; } //mi llave primaria
+        public string Nombre { get; set; } = string.Empty;
+        public decimal Precio { get; set; } 
+    }
+}
